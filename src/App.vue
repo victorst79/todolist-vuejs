b4dca779dc997ceac030cd3e098e9e7788e5b6b0
unknown <victorst79@gmail.com>
2019-01-23 09:20:05 +0100

    add boostrap4 and themes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">
+				<img src="./assets/logo_todo.jpg">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#"><router-link to="/">Home</router-link></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><router-link to="/about">About</router-link></a>
+				</li>
+				</ul>
+			</div>
+		</nav>
+    </div>    
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+nav img{
+	width: 50px;
+	height: 60px;
 }
 </style>
